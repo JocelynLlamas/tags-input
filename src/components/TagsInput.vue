588dc321c1d33e6d7bef180d0bdead08af73e158
JocelynLlamas <70532285+JocelynLlamas@users.noreply.github.com>
2023-11-14 17:54:49 -0600

    Styling the tags input component

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -1,70 +1,109 @@
 <template>
+  <div class="tags-input-wrapper">
     <div>{{ tags }}</div>
     <!-- v directives -->
     <div v-text="tags.length"></div>
-    <div v-if="!tags.length">
-        You have no any tags. Enter it in the input
-    </div>
-    <div v-for="(tag, index) in tags">
-        {{ tag }}
-        <a @click.prevent="removeTag(index)" href="#">&times;</a>
-    </div>
-    
-    <hr/>
+    <div v-if="!tags.length">You have no any tags. Enter it in the input</div>
+    <span class="tag-item" v-for="(tag, index) in tags" :key="index">
+      {{ tag }}
+      <a class="remove-tag" @click.prevent="removeTag(index)" href="#">&times;</a>
+    </span>
+
+    <hr />
     {{ newTag }}
 
-    <input 
-        type="text" 
-        v-model.trim="newTag"
-        @keydown.enter="addNewTag"
-        @keydown.delete="removeLastTag"
-        @keydown.tab.prevent="addNewTag"
-        :class="{ 'tag-exists': isTagExists }"
+    <input
+      class="tag-input"
+      type="text"
+      v-model.trim="newTag"
+      @keydown.enter="addNewTag"
+      @keydown.delete="removeLastTag"
+      @keydown.tab.prevent="addNewTag"
+      :class="{ 'tag-exists': isTagExists }"
     />
+  </div>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        tags: ["vue", "react", "angular"],
-        newTag: "preact"
-        // tags: []
-    }),
-    watch: {
-        newTag(newVal){
-            if (newVal.indexOf(",") > -1) {
-                this.newTag = this.newTag.slice(0, -1)
-                this.addNewTag();
-            }
-        }
+  data: () => ({
+    tags: ["vue", "react", "angular"],
+    newTag: "preact",
+    // tags: []
+  }),
+  watch: {
+    newTag(newVal) {
+      if (newVal.indexOf(",") > -1) {
+        this.newTag = this.newTag.slice(0, -1);
+        this.addNewTag();
+      }
     },
-    computed: {
-        isTagExists(){
-            return this.tags.includes(this.newTag)
-        }
+  },
+  computed: {
+    isTagExists() {
+      return this.tags.includes(this.newTag);
     },
-    methods: {
-        addNewTag(){
-            if (this.newTag && !this.isTagExists) {
-                this.tags.push(this.newTag)
-                this.newTag=""
-            }
-        },
-        removeTag(index){
-            this.tags.splice(index, 1)
-        },
-        removeLastTag(){
-            if (this.newTag.length === 0) {
-                this.removeTag(this.tags.length-1)
-            }
-        }
+  },
+  methods: {
+    addNewTag() {
+      if (this.newTag && !this.isTagExists) {
+        this.tags.push(this.newTag);
+        this.newTag = "";
+      }
     },
-}
+    removeTag(index) {
+      this.tags.splice(index, 1);
+    },
+    removeLastTag() {
+      if (this.newTag.length === 0) {
+        this.removeTag(this.tags.length - 1);
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-.tag-exists{
-    color: red;
-    text-decoration: line-through
+.tag-input.tag-exists {
+  color: red;
+  text-decoration: line-through;
 }
+.tags-input-wrapper {
+  background: #fff;
+  padding: 0.5em;
+  border: 1px solid #dbdbdb;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  min-height: 36px;
+  box-sizing: border-box;
+}
+.tag-item {
+  color: #212529;
+  background-color: #eee;
+  margin-right: 0.3em;
+  padding: 0.25em 0.4em;
+  font-size: 75%;
+  line-height: 1;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding-right: 1.25em;
+  padding-left: 0.6em;
+}
+.tag-input {
+  color: #495057;
+  flex: 1;
+  background: transparent;
+  border: none;
+}
+.tag-input:focus {
+  outline: none;
+}
+a.remove-tag {
+  text-decoration: none;
+}
+
+
 </style>
