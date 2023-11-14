31c6430e1b96124a327622ccb2d4d6e7839517bf
JocelynLlamas <70532285+JocelynLlamas@users.noreply.github.com>
2023-11-14 17:03:10 -0600

    Handling events

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -7,7 +7,12 @@
     </div>
     <div v-for="(tag, index) in tags">{{ index + ":" + tag }}</div>
     
-    <input type="text" :value="newTag"/>
+    <input 
+        type="text" 
+        :value="newTag" 
+        @keydown.enter="tags.push($event.target.value)"
+        @keydown.tab.prevent="tags.push($event.target.value)"
+    />
 </template>
 
 <script>
