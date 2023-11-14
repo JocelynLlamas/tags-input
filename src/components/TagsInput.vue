c023e7a8e9d68cd7bf6180f3e2516fd33a730b54
JocelynLlamas <70532285+JocelynLlamas@users.noreply.github.com>
2023-11-14 16:52:16 -0600

    Directiives and Attributes bindings

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -1,3 +1,21 @@
 <template>
-    <input type="text" />
+    <div>{{ tags }}</div>
+    <!-- v directives -->
+    <div v-text="tags.length"></div>
+    <div v-if="!tags.length">
+        You have no any tags. Enter it in the input
+    </div>
+    <div v-for="(tag, index) in tags">{{ index + ":" + tag }}</div>
+    
+    <input type="text" :value="newTag"/>
 </template>
+
+<script>
+export default {
+    data: () => ({
+        tags: ["vue", "react", "angular"],
+        newTag: "preact"
+        // tags: []
+    }) 
+}
+</script>
